ef09e34252a46af85d67c522e89b52b47bb65a96
gywlsp <gywls517@gmail.com>
2021-05-14 01:11:48 +0900

    chore: :bento: add pin image links

---
+++ src/assets/images/pins.ts
@@ -0,0 +1,4 @@
+export const userPin =
+  'https://user-images.githubusercontent.com/47051596/118152074-43edb500-b44f-11eb-8eb9-5843b4179f60.png';
+export const starPin =
+  'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png';
