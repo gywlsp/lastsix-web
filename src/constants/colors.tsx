a583d8443bc77ad8ab1db2cbfa43d6dcc9139f72
gywlsp <gywls517@gmail.com>
2021-05-13 15:18:30 +0900

    chore: :lipstick: add colors

---
+++ src/constants/colors.tsx
@@ -0,0 +1,18 @@
+export const WHITE = '#FFF';
+export const BLACK = '#000';
+export const GREY = {
+  100: '#FAFAFA',
+  200: '#F5F5F5',
+  300: '#E8E8E8',
+  400: '#D8D8D8',
+  500: '#AAA',
+  600: '#757575',
+  700: '#5D5D5D',
+  800: '#333',
+  900: '#111',
+};
+
+export const IVORY = '#F4F1ED';
+export const MINT = '#43C8BA';
+export const NAVY = '#435CC8';
+export const PURPLE = '#8104AE';
