5e99e2008b759d971930a92f3fe267a8ae2dd1cd
gywlsp <gywls517@gmail.com>
2021-05-15 23:01:44 +0900

    chore: :truck: move codes in Kakaomap Component to Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import styled from 'styled-components';
 
-import KakaoMap from 'src/components/kakaomap';
+import GlobalLayout from 'src/layouts/global';
+import LoadingOverlay from 'src/components/kakaomap/loading/overlay';
+import LocationDetailModal from 'src/components/kakaomap/location-modal';
 
-export default function Home() {
-  return <KakaoMap />;
+import { useMapContext, withMapContext } from 'src/contexts/map';
+
+function Home() {
+  const {
+    state: { latitude, longitude },
+  } = useMapContext();
+
+  return (
+    <GlobalLayout>
+      {!(latitude && longitude) && <LoadingOverlay />}
+      <LocationDetailModal />
+      <Map id="kakaomap" />
+    </GlobalLayout>
+  );
 }
+
+export default withMapContext(Home);
+
+const Map = styled.div`
+  width: 100vw;
+  height: 100vh;
+`;
